853002d8d7601146aac6332cd3c0f05db84cd757
Piotr Podlaski <piotrek@post.kek.jp>
2025-06-12 22:05:49 +0900

    Added argparse and prepare for GUI config file

--- GUI/rimokunControl.cpp
+++ GUI/rimokunControl.cpp
@@ -2,8 +2,38 @@
 #include <logger.hpp>
 
 #include "MainWindow.hpp"
+#include "argparse/argparse.hpp"
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+
 int main(int argc, char* argv[]) {
   utl::configure_logger();
+  argparse::ArgumentParser program("rimokunControl");
+  program.add_argument("-c", "--config")
+      .help("PAth to the config file")
+      .default_value("/etc/rimokunControl.yaml");
+
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::exception& err) {
+    SPDLOG_CRITICAL("{}", err.what());
+    std::exit(1);
+  }
+
+  const auto configPath = program.get<std::string>("-c");
+
+  if (!program.is_used("-c")) {
+    SPDLOG_WARN("Config path not provided, using default: {}", configPath);
+  }
+
+  if (!fs::exists(configPath)) {
+    SPDLOG_CRITICAL("Config file '{}' not found! Exiting.", configPath);
+    std::exit(1);
+  }
+
   QApplication a(argc, argv);
   QApplication::setStyle("Fusion");
   MainWindow w;

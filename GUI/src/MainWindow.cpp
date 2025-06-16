0855cbaae23ff21db739d63a5a6fd3ca8d69a831
Piotr Podlaski <piotrek@post.kek.jp>
2025-06-16 09:34:30 +0900

    gui config cleanup

--- GUI/src/MainWindow.cpp
+++ GUI/src/MainWindow.cpp
@@ -37,10 +37,6 @@ MainWindow::MainWindow(QWidget* parent)
   updater.startUpdaterThread();
 
   connect(&updater, &Updater::newDataArrived, ui->widget, &RobotVisualisation::updateRobotPosition);
-
-  QTimer::singleShot(1000, [&]() { ui->widget->moveRedTo({0, 0}); });
-  //QTimer::singleShot(2000, [&]() { ui->widget->moveGreenTo({200, 200}); });
-  //QTimer::singleShot(3000, [&]() { ui->widget->moveBlueTo({300, 200}); });
 }
 
 MainWindow::~MainWindow() { delete ui; }

7c22692a1f259e76e4e98f1fbed75618f229f925
Piotr Podlaski <piotrek@post.kek.jp>
2026-02-16 16:16:47 +0900

    Add concurrent command queue stress tests

--- tests/unit/server/CommandQueueTests.cpp
+++ tests/unit/server/CommandQueueTests.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <future>
 #include <thread>
+#include <vector>
 
 using namespace std::chrono_literals;
 
@@ -58,4 +59,65 @@ TEST(CommandQueueTests, PushAfterShutdownIsRejected) {
   queue.shutdown();
 
   EXPECT_FALSE(queue.push(makeReconnectCommand()));
+}
+
+TEST(CommandQueueTests, MultipleProducersAllCommandsAreConsumed) {
+  cmd::CommandQueue queue;
+  constexpr int producerCount = 4;
+  constexpr int commandsPerProducer = 200;
+  constexpr int totalCommands = producerCount * commandsPerProducer;
+
+  std::vector<std::thread> producers;
+  producers.reserve(producerCount);
+  for (int p = 0; p < producerCount; ++p) {
+    producers.emplace_back([&]() {
+      for (int i = 0; i < commandsPerProducer; ++i) {
+        ASSERT_TRUE(queue.push(makeReconnectCommand()));
+      }
+    });
+  }
+
+  int consumed = 0;
+  while (consumed < totalCommands) {
+    auto popped = queue.pop_wait_for(500ms);
+    ASSERT_TRUE(popped.has_value());
+    ++consumed;
+  }
+
+  for (auto& producer : producers) {
+    producer.join();
+  }
+
+  EXPECT_EQ(consumed, totalCommands);
+  EXPECT_FALSE(queue.try_pop().has_value());
+}
+
+TEST(CommandQueueTests, ShutdownRejectsNewPushesUnderLoad) {
+  cmd::CommandQueue queue;
+  std::atomic<int> accepted{0};
+  std::atomic<int> rejected{0};
+
+  std::thread producer([&]() {
+    for (int i = 0; i < 10000; ++i) {
+      if (queue.push(makeReconnectCommand())) {
+        accepted.fetch_add(1, std::memory_order_relaxed);
+      } else {
+        rejected.fetch_add(1, std::memory_order_relaxed);
+        break;
+      }
+      std::this_thread::sleep_for(50us);
+    }
+  });
+
+  std::this_thread::sleep_for(5ms);
+  queue.shutdown();
+  producer.join();
+
+  EXPECT_GT(rejected.load(std::memory_order_relaxed), 0);
+
+  int drained = 0;
+  while (queue.try_pop().has_value()) {
+    ++drained;
+  }
+  EXPECT_EQ(drained, accepted.load(std::memory_order_relaxed));
 }
